aa8c0900f771584895ac04c9b23f54b91d41d189
Fuad-Khan <107122588+Fuad-Khan@users.noreply.github.com>
2022-06-21 14:55:54 +0600

    Add files via upload

---
+++ Lab Exersise/lab exersise 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int c,d;
+
+
+	printf("Enter the value of c:");
+	scanf("%d",&c);
+	printf("Enter the value of d:");
+	scanf("%d",&d);
+	printf("the value of c=%d\nthe value of d=%d",d,c);
+
+	return 0;
+
+}
